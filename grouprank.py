9fbecf866d9df854f4b2b2751cefccba14600414
daisy.ma <2452825107@qq.com>
2024-11-29 17:07:45 +0800

    clean

--- grouprank.py
+++ grouprank.py
@@ -33,11 +33,6 @@ class Rank:
                 type_ = cur_type,
                 day = rank.RankDayType.MONTH
             ))
-            # print(lv_rank_dict)
-            # file.func_write_to_json(
-            #     json_file_name=f'./rank_data.json',
-            #     json_dict=lv_rank_dict,
-            # )
             lv_rank_list: List = lv_rank_dict["list"]
 
             lv_rank_data = ""
@@ -49,16 +44,9 @@ class Rank:
                 lv_rank_data += f'播放量：{lv_view}\n'
             lv_rank_data += f'sum_{cur_type}: {all_views}'
             cur_rid = f'{cur_type}'
-
-            # file_name = f'./{cur_rid}_rank_data.txt '
-            # file.func_write_to_file(
-            #     file_name=file_name,
-            #     msg=lv_rank_data,
-            # )
             cur_type_str = str(cur_type)
             cur_type_name = cur_type_str.split('.')[-1]
             total_views[f"{cur_type_name}"] = all_views
-            # total_views[f"{cur_type_name} {all_views}"] = all_views
 
         file.func_write_to_json(
             json_file_name='./total_views.json',
